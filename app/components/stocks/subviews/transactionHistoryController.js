93d6df5125c4feb8fd2d95d8c66e1f90a3a712c3
Gareth Daunton <gdaunton@gmail.com>
2016-05-07 22:36:56 -0400

    transaction stuff, not sure what it is.

--- app/components/stocks/subviews/transactionHistoryController.js
+++ app/components/stocks/subviews/transactionHistoryController.js
@@ -3,7 +3,6 @@ var TransactionHistoryController = angular.module("TransactionHistoryController"
 TransactionHistoryController.controller('TransactionHistoryController',
     ['$scope', '$window', 'StockService', 'AuthService', 'CaveWallAPIService',
         function ($scope, $window, stockService, authService, api) {
-          $('.button.action').popup();
           $scope.transactions = [];
           api.makeCall('GET', 'users/financialtransactions', 'all', null, function(transactions) {
             transactions.forEach(function(item, index) {
