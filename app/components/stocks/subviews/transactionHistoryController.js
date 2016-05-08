aea5cddf04253fa080fcc1604bbcfe2da2f56246
Gareth Daunton <gdaunton@gmail.com>
2016-05-07 21:47:50 -0400

    Finished transaction history

--- app/components/stocks/subviews/transactionHistoryController.js
+++ app/components/stocks/subviews/transactionHistoryController.js
@@ -3,8 +3,12 @@ var TransactionHistoryController = angular.module("TransactionHistoryController"
 TransactionHistoryController.controller('TransactionHistoryController',
     ['$scope', '$window', 'StockService', 'AuthService', 'CaveWallAPIService',
         function ($scope, $window, stockService, authService, api) {
+          $('.button.action').popup();
           $scope.transactions = [];
-          api.makeCall('GET', 'users/financialtransactions/all', 'all', null, function(transactions) {
+          api.makeCall('GET', 'users/financialtransactions', 'all', null, function(transactions) {
+            transactions.forEach(function(item, index) {
+              item.TransactionDate = new Date(item.TransactionDate);
+            });
             $scope.transactions = transactions;
             $scope.$apply();
           }, function(data) {
